4aceaae54d1e92514b5482931169b4b05a906f17
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2025-11-03 17:24:58 -0700

    Add support for audio files

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|mp3|wav|ogg|m4a)$/i,
         type: "asset/resource",
       },
     ],
